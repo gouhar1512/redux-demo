51aacd8d72b433a72e5e7bb85deb18cedd73673e
gouhar1512 <ushran987@gmail.com>
2023-07-02 13:33:33 +0530

    Implementing store and redux in action

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const redux = require("redux");
+const createStore = redux.createStore;
+
 const BUY_CAKE = "BUY_CAKE";
 
 function buyCake() {
@@ -11,7 +14,7 @@ const initialState = {
   numOfCakes: 10,
 };
 
-const reducer = (state, action) => {
+const reducer = (state = initialState, action) => {
   switch (action.type) {
     case BUY_CAKE:
       return {
@@ -19,6 +22,29 @@ const reducer = (state, action) => {
         numOfCakes: state.numOfCakes - 1,
       };
     default:
+      console.log("default action dispatched");
       return state;
   }
 };
+
+const store = createStore(reducer);
+console.log("Initial State", store.getState());
+
+const unsubscribe = store.subscribe(() => {
+  console.log("Updated state", store.getState());
+});
+store.dispatch(buyCake());
+store.dispatch(buyCake());
+store.dispatch(buyCake());
+unsubscribe();
+
+/**
+ * REDUX PATTERN
+ * ==============
+ * Create a store
+ * Declare the initial state and the reducer
+ * Define your actions and action creators
+ * Subscribe to the store
+ * Dispatch actions to update the store
+ * Unsubscribe to the changes
+ */

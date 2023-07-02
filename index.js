0602d8cee9dec461a20955ac4c9bf0d7f7902c46
gouhar1512 <ushran987@gmail.com>
2023-07-02 13:05:03 +0530

    Implementing Reducers

--- index.js
+++ index.js
@@ -6,3 +6,19 @@ function buyCake() {
     info: "First redux action",
   };
 }
+
+const initialState = {
+  numOfCakes: 10,
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case BUY_CAKE:
+      return {
+        ...state,
+        numOfCakes: state.numOfCakes - 1,
+      };
+    default:
+      return state;
+  }
+};
